273ab053fcb912880bbcfe8ced452485570801b0
fabianhaef <fabian.haefi@gmail.com>
2023-09-06 20:46:15 +0200

    create ScreenNavigation props for navigation

---
+++ types.ts
@@ -0,0 +1,10 @@
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+    Home: undefined;
+    EditNote: undefined;
+}
+
+export type ScreenNavigationProp = NativeStackNavigationProp<
+    RootStackParamList
+>;
